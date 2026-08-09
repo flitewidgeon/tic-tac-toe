168a4828bc31a5f30009da8017ad0ad8491d020c
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2026-08-09 20:24:51 +1200

    Add further subarrays to identify board columns and diagonals

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ function gameBoard(){
     const middleRow = [squares.midLeft, squares.center, squares.midRight];
     const bottomRow = [squares.bottomLeft, squares.bottomCenter, squares.bottomRight];
 
+    const leftCol = [squares.topLeft, squares.midLeft, squares.bottomLeft];
+    const middleCol = [squares.topCenter, squares.center, squares.bottomCenter];
+    const rightCol = [squares.topRight, squares.midRight, squares.bottomRight];
+
+    const leftDiagonal = [squares.topLeft, squares.center, squares.bottomRight];
+    const rightDiagonal = [squares.topRight, squares.center, squares.bottomLeft];   
+
     function checkLine(line, marker){
         if (line[0] == marker &&
             line[1] == marker &&
