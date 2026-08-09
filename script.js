4a957cf4d1a6bc8a47928f5c6ab8d57e57c0d084
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2026-08-09 20:23:45 +1200

    Create gameboard object with array for the board itself, a helper object to identify board squares in plain English, subarrays to identify board rows and a function to check a line of 3 board squares

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ function player(marker){
 }
 
 
+function gameBoard(){
+    let grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const squares = {
+        topLeft: grid[0],
+        topCenter: grid[1],
+        topRight: grid[2],
+
+        midLeft: grid[3],
+        center: grid[4],
+        midRight: grid[5],
+
+        bottomLeft: grid[6],
+        bottomCenter: grid[7],
+        bottomRight: grid[8] 
+    };
+
+    const topRow = [squares.topLeft, squares.topCenter, squares.topRight];
+    const middleRow = [squares.midLeft, squares.center, squares.midRight];
+    const bottomRow = [squares.bottomLeft, squares.bottomCenter, squares.bottomRight];
+
+    function checkLine(line, marker){
+        if (line[0] == marker &&
+            line[1] == marker &&
+            line[2] == marker){
+            return true;
+    }
+    return false;
+}
+
+return{grid, checkLine};
+}    
