9c77241b81b9e9ace45646646b77b7bdc6c1f7c7
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2026-08-11 19:56:17 +1200

    Moved the subarrays describing the lines of the grid into the checkWin function as they need to be created each time it is called

--- script.js
+++ script.js
@@ -6,66 +6,74 @@ function player(marker){
 function gameBoard(){
     const grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    const squares = {
-        topLeft: grid[0],
-        topCenter: grid[1],
-        topRight: grid[2],
 
-        midLeft: grid[3],
-        center: grid[4],
-        midRight: grid[5],
 
-        bottomLeft: grid[6],
-        bottomCenter: grid[7],
-        bottomRight: grid[8] 
-    };
+    function checkWin(marker){
+        const squares = {
+            topLeft: grid[0],
+            topCenter: grid[1],
+            topRight: grid[2],
 
-    const topRow = [squares.topLeft, squares.topCenter, squares.topRight];
-    const middleRow = [squares.midLeft, squares.center, squares.midRight];
-    const bottomRow = [squares.bottomLeft, squares.bottomCenter, squares.bottomRight];
+            midLeft: grid[3],
+            center: grid[4],
+            midRight: grid[5],
 
-    const leftCol = [squares.topLeft, squares.midLeft, squares.bottomLeft];
-    const middleCol = [squares.topCenter, squares.center, squares.bottomCenter];
-    const rightCol = [squares.topRight, squares.midRight, squares.bottomRight];
+            bottomLeft: grid[6],
+            bottomCenter: grid[7],
+            bottomRight: grid[8] 
+        };
 
-    const leftDiagonal = [squares.topLeft, squares.center, squares.bottomRight];
-    const rightDiagonal = [squares.topRight, squares.center, squares.bottomLeft];   
+        const topRow = [squares.topLeft, squares.topCenter, squares.topRight];
+        const middleRow = [squares.midLeft, squares.center, squares.midRight];
+        const bottomRow = [squares.bottomLeft, squares.bottomCenter, squares.bottomRight];
 
-    function checkLine(line, marker){
-        if (line[0] == marker &&
-            line[1] == marker &&
-            line[2] == marker){
-            return true;
+        const leftCol = [squares.topLeft, squares.midLeft, squares.bottomLeft];
+        const middleCol = [squares.topCenter, squares.center, squares.bottomCenter];
+        const rightCol = [squares.topRight, squares.midRight, squares.bottomRight];
+
+        const leftDiagonal = [squares.topLeft, squares.center, squares.bottomRight];
+        const rightDiagonal = [squares.topRight, squares.center, squares.bottomLeft];   
+
+        function checkLine(line, marker){
+            if (line[0] == marker &&
+                line[1] == marker &&
+                line[2] == marker){
+                return true;
         }
         return false;
     }
+    return checkLine(topRow, marker) || checkLine(middleRow, marker) || checkLine(bottomRow, marker)||
+    checkLine(leftCol, marker)|| checkLine(middleCol, marker) || checkLine(rightCol, marker) ||
+    checkLine(leftDiagonal, marker) || checkLine(rightDiagonal, marker);
+}
 
-    function checkWin(marker){
-        return checkLine(topRow, marker) || checkLine(middleRow, marker) || checkLine(bottomRow, marker)||
-        checkLine(leftCol, marker)|| checkLine(middleCol, marker) || checkLine(rightCol, marker) ||
-        checkLine(leftDiagonal, marker) || checkLine(rightDiagonal, marker);
+function checkSquareEmpty(position){
+    return grid[position] === 0;
+}
+
+function addMarker(player, position){
+    grid[position] = player.marker;
+}
+
+function reset(){
+    for (let i = 0; i < grid.length; i++){
+        grid[i] = 0;
     }
+}
 
-    function checkSquareEmpty(position){
-        return grid[position] === 0;
-    }
+function checkSquaresAvailable(){
+    return grid.includes(0);
+}
 
-    function addMarker(player, position){
-        grid[position] = player.marker;
-    }
-
-    function reset(){
-        for (let i = 0; i < grid.length; i++){
-            grid[i] = 0;
-        }
-    }
-
-    function checkSquaresAvailable(){
-        return grid.includes(0);
-    }
-
-    return{grid, checkSquareEmpty, checkSquaresAvailable, checkWin, addMarker, reset};
+return{grid, checkSquareEmpty, checkSquaresAvailable, checkWin, addMarker, reset};
 }    
+
+board = gameBoard();
+player = player('X');
+board.addMarker(player, 0);
+board.addMarker(player, 1);
+board.addMarker(player, 2);
+console.log(board.checkWin(player.marker));
 
 
 function game(){
@@ -87,5 +95,4 @@ function game(){
 
 }
 
-game();
 
