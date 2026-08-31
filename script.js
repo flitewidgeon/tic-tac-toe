d86242d01f132c53d95ed76cc801383bf603fac4
flitewidgeon <197684491+flitewidgeon@users.noreply.github.com>
2026-08-31 20:19:32 +1200

    Extracted event listener code from function game.turn(). Each call to the function had created a new event listener with the result that the code was being run multiple times in response to a click event. Grid squares were being marked as not empty as a result.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function player(name, marker) {
-    return { name, marker };
+
+    function setName(newName) {
+        name = newName;
+    }
+
+    function getMarker() {
+        return marker;
+    }
+
+    return { name, setName, getMarker };
 }
 
 const gameBoard = (function () {
     const grid = ['', '', '', '', '', '', '', '', ''];
 
-    function getGrid(){
+    function getGrid() {
         return grid;
     }
 
@@ -44,11 +53,11 @@ const gameBoard = (function () {
     }
 
     function checkSquareEmpty(position) {
-        return grid[position] === '';
+        return grid[position] == '';
     }
 
     function addMarker(player, position) {
-        grid[position] = player.marker;
+        grid[position] = player.getMarker();
     }
 
     function reset() {
@@ -61,61 +70,79 @@ const gameBoard = (function () {
         return grid.includes('');
     }
 
-    return { getGrid, checkSquareEmpty, checkSquaresAvailable, checkWin, addMarker, reset };
+    return { getGrid, checkSquareEmpty, checkSquaresAvailable, checkWin, reset, addMarker };
 }());
 
-function game() {
+const game = (function () {
+    const playerOne = player('Player One', 'O');
+    const playerTwo = player('Player Two', 'X');
 
-    const playerOne = player('playerOne', 'O');
-    const playerTwo = player('playerTwo', 'X');
-    let running = true;
+
+
+    let running = false;
+
+    function getPlayerOne() {
+        return playerOne;
+    }
+
+    function getPlayerTwo() {
+        return playerTwo;
+    }
 
     function randomNum(num) {
         return Math.floor(Math.random() * num);
     }
 
-    function play() {
-        // Randomly choose player one or player two to go first
-        const startPlayer = randomNum(2) === 0 ? playerOne : playerTwo;
-        turn(startPlayer, displayController.gridContainer);
+    function start() {
+        running = true;
     }
 
-    function turn(player, gridContainer) {
-        gridContainer.addEventListener('click', (event) => {
-            const position = event.target.id;
-            // if there is no winner, and there are squares available then process the click
-            if (running) {
-                if (gameBoard.checkSquareEmpty(position)) {
-                    gameBoard.addMarker(player, position);
-                    displayController.update(gameBoard);
+    // Randomly choose player one or player two to go first
+    let currentPlayer = randomNum(2) === 0 ? playerOne : playerTwo;
 
-                    // keep running while a player has not won 
-                    if (gameBoard.checkWin(player.marker, position)) {
-                        running = false;
-                        console.log(`${player.name} has won!`);
-                    }
-                    else if (!gameBoard.checkSquaresAvailable()){
-                        running = false;
-                        console.log("It's a draw");
-                    }
-                    // toggle player turn
-                    player = player == playerOne ? playerTwo : playerOne;
-                }
-                else {
-                    console.log('That square is occupied!');
-                }
+    function turn(position) {
+        // if there is no winner, and there are squares available then process the click
+        if (running) {
+            if (gameBoard.checkSquareEmpty(position)) {
+                gameBoard.addMarker(currentPlayer, position);
+                displayController.update();
             }
-        });
+            else {
+                console.log('That square is occupied!');
+            }
+
+            // keep running while a player has not won 
+            if (gameBoard.checkWin(currentPlayer.getMarker(), position)) {
+                running = false;
+                console.log(`${currentPlayer.name} has won!`);
+            }
+            else if (!gameBoard.checkSquaresAvailable()) {
+                running = false;
+                console.log("It's a draw");
+            }
+            // toggle player turn
+            currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+
+        }
+
     }
 
-    return { play };
-}
+    return { start, getPlayerOne, getPlayerTwo, turn };
+}());
 
 const displayController = (function () {
     const gridContainer = document.querySelector('.grid-container');
     const squareList = document.querySelectorAll('.grid-container > div');
 
-    function update(gameBoard) {
+    function getGridContainer() {
+        return gridContainer;
+    }
+
+    function getSquareList() {
+        return squareList;
+    }
+
+    function update() {
         const grid = gameBoard.getGrid();
         // the grid and the squareList each have 9 squares
         for (let i = 0; i < grid.length; i++) {
@@ -123,10 +150,49 @@ const displayController = (function () {
         }
     }
 
+    squareList.forEach(square => {
+        square.addEventListener('click', (event) => {
+            const position = event.target.id;
+            game.turn(position);
+        });
+    });
 
-    return { gridContainer, squareList, update };
+
+    return { getGridContainer, getSquareList, update };
 
 }());
 
-game = game();
-game.play();
+
+const newGameButton = document.querySelector('#new-game');
+const closeButton = document.querySelector('#close-button');
+const submitButton = document.querySelector('#submit-button');
+const dialog = document.querySelector('dialog');
+
+closeButton.addEventListener('click', () => dialog.close());
+
+newGameButton.addEventListener('click', () => dialog.showModal());
+
+submitButton.addEventListener('click', () => {
+
+    const playerOneInput = document.querySelector('#player-one').value;
+    const playerTwoInput = document.querySelector('#player-two').value;
+
+    // get the player objects 
+    const playerOne = game.getPlayerOne();
+    const playerTwo = game.getPlayerTwo();
+    // Set the name of the players with a default name or user entered name
+    const playerOneName = playerOneInput == null || playerOneInput == '' ? 'Player One' : playerOneInput;
+    playerOne.setName(playerOneName);
+    const playerTwoName = playerTwoInput == null || playerTwoInput == '' ? 'Player Two' : playerTwoInput;
+    playerTwo.setName(playerTwoName);
+
+    const dialog = document.querySelector('dialog');
+    dialog.close();
+
+    // reset the game
+    gameBoard.reset();
+    displayController.update();
+
+    // start the game
+    game.start();
+})
